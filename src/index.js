99fc2ab26544e2a6354aadfd0767d2899ed8c33f
mana-sumiyoshi <89341177+mana-sumiyoshi@users.noreply.github.com>
2021-08-27 10:01:36 +0900

    入力値の取得

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import "./styles.css";
+
+const addButton = document.getElementById("addButton");
+addButton.addEventListener("click", () => {
+  const addText = document.getElementById("addText");
+  const inputText = addText.value;
+  addText.value = "";
+
+  const li = document.createElement("li");
+});
+
+console.log();
